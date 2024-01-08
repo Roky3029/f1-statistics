708bb9b6ace7cd26117dc4a13160aab2824fbd9c
Roky3029 <mikirb30@gmail.com>
2024-01-08 23:22:33 +0100

    v2.0.2 -> First post and some fixes

--- src/data/newData/getNextRace.ts
+++ src/data/newData/getNextRace.ts
@@ -1,17 +1,17 @@
-import { NextEvent } from '@/types/next_event'
-import { SECONDS_ISR, SERVER_LINK } from './../consts'
-import { getCircuits } from './getCircuits'
+// import { NextEvent } from '@/types/next_event'
+// import { SECONDS_ISR, SERVER_LINK } from './../consts'
+// import { getCircuits } from './getCircuits'
 
-export const getNextRace = async () => {
-	const url = `${SERVER_LINK}/next_event`
-	const nextRace = await fetch(url, { next: { revalidate: SECONDS_ISR } })
-	const nextRaceData = (await nextRace.json()) as NextEvent
+// export const getNextRace = async () => {
+// 	const url = `${SERVER_LINK}/next_event`
+// 	const nextRace = await fetch(url, { next: { revalidate: SECONDS_ISR } })
+// 	const nextRaceData = (await nextRace.json()) as NextEvent
 
-	const circuits = await getCircuits()
+// 	const circuits = await getCircuits()
 
-	const circuit = circuits.filter(
-		cir => cir.Location.country === nextRaceData.Country
-	)
+// 	const circuit = circuits.filter(
+// 		cir => cir.Location.country === nextRaceData.Country
+// 	)
 
-	return { nextRaceData, circuit }
-}
+// 	return { nextRaceData, circuit }
+// }
